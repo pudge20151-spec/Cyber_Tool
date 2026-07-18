a1a35a9abef35ec39fb247bc27708979063c3894
ScamCheck <pudge20151@gmail.com>
2026-07-18 23:29:47 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 title CyberTool v2.0
 cd /d "%~dp0"
 echo =======================================
-echo  Starting CyberTool v2.0...
+echo  Starting CyberTool :D
 echo =======================================
 C:\Users\123\AppData\Local\Programs\Python\Python312\python.exe main.py
 pause
